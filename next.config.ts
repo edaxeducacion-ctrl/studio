28d69a7bfac5c4ff3e775e6527df68be5fbf032b
edax online <edax.educacion@gmail.com>
2025-11-30 01:57:23 +0000

    esta imagen de fondo https://www.infoblancosobrenegro.com/uploads/notici

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.infoblancosobrenegro.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
