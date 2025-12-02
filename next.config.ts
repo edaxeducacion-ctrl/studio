69e7390d5d631e062348c18c2ec7f69776356fe2
edax online <edax.educacion@gmail.com>
2025-12-02 18:19:33 +0000

    pon esta imagen en Endolifting / Lipólisis  https://encrypted-tbn0.gstat

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,12 @@ const nextConfig: NextConfig = {
         hostname: 'www.infoblancosobrenegro.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
