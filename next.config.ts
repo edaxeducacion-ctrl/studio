468e8f5b794069d0fa783a7bcf7fe5d3a7ff9e99
edax online <edax.educacion@gmail.com>
2025-12-02 18:56:49 +0000

    https://gynemid.mx/wp-content/uploads/2023/09/Rejuvenecimiento-Vaginal-2

--- next.config.ts
+++ next.config.ts
@@ -51,6 +51,12 @@ const nextConfig: NextConfig = {
         hostname: 'www.edwardspodologos.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'gynemid.mx',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
